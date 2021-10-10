59146c9fde2889b46524905afb417fe29c0960f7
S Sathiya <codewithsathya@gmail.com>
2021-10-10 21:08:59 +0530

    test

--- client/src/components/sections/Register.js
+++ client/src/components/sections/Register.js
@@ -99,7 +99,7 @@ function Register({ workshop, workshops, handleClose }) {
     }
     axios
       .post(
-        "https://sanchaar-api.herokuapp.com//api/register/initiateRegistration",
+        "https://wiss-sanchaar-2022.herokuapp.com/api/register/initiateRegistration",
         values
         // {
         //   withCredentials: true,
